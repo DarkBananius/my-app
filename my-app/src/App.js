3151a7b37a82b31f7632c3f6fc196929b92eef20
Islamov <diablomen3@gmail.com>
2023-10-13 16:40:06 +0800

    jsx vs js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,27 +2,66 @@ import logo from './logo.svg';
 import './App.css';
 import { createElement } from 'react';
 
+// export const App = () => {
+// 	let today = new Date(); // декларативный
+// 	let year = today.getFullYear();
+// 	return (
+// 		<div className="App">
+// 			<header className="App-header">
+// 				<img src={logo} className="App-logo" alt="logo" />
+// 				<p>
+// 					Edit <code>src/App.js</code> and save to reload.
+// 				</p>
+// 				<a
+// 					className="App-link"
+// 					href="https://reactjs.org"
+// 					target="_blank"
+// 					rel="noopener noreferrer"
+// 				>
+// 					Learn React
+// 				</a>
+// 				<div>{year}</div>
+// 			</header>
+// 		</div>
+// 	);
+// };
+
 export const App = () => {
 	let today = new Date(); // декларативный
 	let year = today.getFullYear();
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<div>{year}</div>
-			</header>
-		</div>
+	return React.createElement(
+		"div",
+		{
+			className: "App"
+		},
+		React.createElement(
+			"header",
+			{
+				className: "App-header"
+			},
+			React.createElement("img", {
+				src: logo,
+				className: "App-logo",
+				alt: "logo"
+			}),
+			React.createElement(
+				"p",
+				null,
+				"Edit ",
+				React.createElement("code", null, "src/App.js"),
+				" and save to reload."
+			),
+			React.createElement(
+				"a",
+				{
+					className: "App-link",
+					href: "https://reactjs.org",
+					target: "_blank",
+					rel: "noopener noreferrer"
+				},
+				"Learn React"
+			),
+			React.createElement("div", null, year)
+		)
 	);
 };
-
