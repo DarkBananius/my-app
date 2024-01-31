70ad7580c8ea0ba4c9c4e14a0f0f12e7e4c33c50
DarkBananius <Diablomen3@gmail.com>
2024-01-31 20:57:52 +0800

    Реализовать основной компонет блога с роутингом

---
+++ my-app/src/blog.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+import { Routes, Route } from 'react-router-dom';
+import styles from './app.module.css';
+import styled from 'styled-components';
+
+const Content = styled.div`
+    padding: 120px 0;
+`;
+const H2 = styled.h2`
+    text-align: center;
+`;
+
+const Header = () => <div>Шапка</div>
+const Footer = () => <div>Подвал</div>
+
+export const Blog = () => {
+return (
+    <>
+    <Header />
+    <Content>
+        <H2>Контент страницы</H2>
+        <Routes>
+            <Route path='/' element={<div>Главная страинцы</div>} />
+            <Route path='/login' element={<div>Авторизация</div>} />
+            <Route path='/register' element={<div>Регистрация</div>} />
+            <Route path='/users' element={<div>Пользователи</div>} />
+            <Route path='/post' element={<div>Новоя статья</div>} />
+            <Route path='/post/:postId' element={<div>Статья</div>} />
+            <Route path='*' element={<div>Ошибка</div>} />
+        </Routes>
+    </Content>
+    <Footer />
+    </>
+);
+	
+};
+ 
