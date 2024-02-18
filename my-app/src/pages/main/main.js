2c4967f8001110ea8aeb9a7201e44606c643d381
DarkBananius <Diablomen3@gmail.com>
2024-02-18 22:55:15 +0800

    Реализовать поиск на главной

--- my-app/src/pages/main/main.js
+++ my-app/src/pages/main/main.js
@@ -1,42 +1,68 @@
-import { useEffect, useState } from "react";
-import { Pagination, PostCard } from "./components";
+import { useEffect, useMemo, useState } from "react";
 import { useServerRequest } from "../../hooks";
 import { PAGINATION_LIMIT } from "../../constants";
+import { Pagination, PostCard, Search } from "./components";
+import { debounce, getLastPageFromLinks } from "./utils";
 import styled from "styled-components";
-import { getLastPageFromLinks } from "./utils/get-last-page-from-links";
 
 const MainContainer = ({ className }) => {
 	const [posts, setPosts] = useState([]);
 	const [page, setPage] = useState(1);
 	const [lastPage, setLastPage] = useState(1);
+	const [searchPhrase, setSearchPhrase] = useState("");
+	const [shouldSearch, setShouldSearch] = useState(false);
 	const requestServer = useServerRequest();
 
 	useEffect(() => {
-		requestServer("fetchPosts", page, PAGINATION_LIMIT).then(
+		requestServer("fetchPosts", searchPhrase, page, PAGINATION_LIMIT).then(
 			({ res: { posts, links } }) => {
 				setPosts(posts);
 				setLastPage(getLastPageFromLinks(links));
 			},
 		);
-	}, [requestServer, page]);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [requestServer, page, shouldSearch]);
+
+	const startDelayedSearch = useMemo(
+		() => debounce(setShouldSearch, 2000),
+		[],
+	);
+
+	const onSearch = ({ target }) => {
+		setSearchPhrase(target.value);
+		startDelayedSearch(!shouldSearch);
+	};
 
 	return (
 		<div className={className}>
-			<div className="post-list">
-				{posts.map(
-					({ id, title, imageUrl, publishedAt, commentsCount }) => (
-						<PostCard
-							key={id}
-							id={id}
-							title={title}
-							imageUrl={imageUrl}
-							publishedAt={publishedAt}
-							commentsCount={commentsCount}
-						/>
-					),
+			<div className="posts-and-search">
+				<Search searchPhrase={searchPhrase} onChange={onSearch} />
+				{posts.length > 0 ? (
+					<div className="post-list">
+						{posts.map(
+							({
+								id,
+								title,
+								imageUrl,
+								publishedAt,
+								commentsCount,
+							}) => (
+								<PostCard
+									key={id}
+									id={id}
+									title={title}
+									imageUrl={imageUrl}
+									publishedAt={publishedAt}
+									commentsCount={commentsCount}
+								/>
+							),
+						)}
+					</div>
+				) : (
+					<div className="no-posts-found">Статьи не найдены</div>
 				)}
 			</div>
-			{lastPage > 1 && (
+			{lastPage > 1 && posts.length > 0 && (
 				<Pagination page={page} lastPage={lastPage} setPage={setPage} />
 			)}
 		</div>
@@ -44,9 +70,18 @@ const MainContainer = ({ className }) => {
 };
 
 export const Main = styled(MainContainer)`
+	margin-top: 40px;
+
 	& .post-list {
 		display: flex;
 		flex-wrap: wrap;
-		padding: 20px;
+		padding: 20px 20px 80px;
+		margin-bottom: 64px;
+	}
+
+	& .no-posts-found {
+		text-align: center;
+		font-size: 18px;
+		margin-top: 40px;
 	}
 `;
